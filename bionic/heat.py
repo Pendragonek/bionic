92f347ee3c779d0b22c127b695f6c071b8293251
Pendragonek <mateuszlekner@gmail.com>
2021-02-08 20:57:17 +0100

    Implement calculating combined temperature

--- bionic/heat.py
+++ bionic/heat.py
@@ -9,3 +9,11 @@ def calculate_heat_amount(shc: float, mass: float, temperature: float) -> float:
 def calculate_temperature_delta(shc: float, mass: float, heat_amount: float) -> float:
     """Calculate temperature delta based on SHC, mass and heat transferred"""
     return heat_amount / (shc * mass)
+
+
+def calculate_combined_temperature(shc1: float, mass1: float, temperature1: float,
+                                   shc2: float, mass2: float, temperature2: float) -> float:
+    """Calculate combined temperature, the result of mixing two elements temperature"""
+    total_heat_amount = calculate_heat_amount(shc1, mass1, temperature1) + calculate_heat_amount(shc2, mass2,
+                                                                                                 temperature2)
+    return total_heat_amount / (shc1 * mass1 + shc2 * mass2)
--- tests/test_heat.py
+++ tests/test_heat.py
@@ -1,5 +1,5 @@
 """Test heat calculations"""
-from bionic.heat import calculate_heat_amount, calculate_temperature_delta
+from bionic.heat import calculate_heat_amount, calculate_temperature_delta, calculate_combined_temperature
 
 
 def test_calculate_heat_amount():
@@ -16,3 +16,36 @@ def test_calculate_temperature_delta():
     heat_amount = 6000
     temperature_delta = calculate_temperature_delta(shc, mass, heat_amount)
     assert temperature_delta == 2.5
+
+
+def test_calculate_combined_temperature():
+    shc1 = 2.400
+    mass1 = 1000
+    temperature1 = 10
+    shc2 = 2.400
+    mass2 = 1000
+    temperature2 = 40
+    combined_temperature = calculate_combined_temperature(shc1, mass1, temperature1, shc2, mass2, temperature2)
+    assert combined_temperature == 25
+
+
+def test_calculate_combined_temperature_different_mass():
+    shc1 = 2.400
+    mass1 = 1000
+    temperature1 = 10
+    shc2 = 2.400
+    mass2 = 4000
+    temperature2 = 40
+    combined_temperature = calculate_combined_temperature(shc1, mass1, temperature1, shc2, mass2, temperature2)
+    assert combined_temperature == 34
+
+
+def test_calculate_combined_temperature_different_mass_and_shc():
+    shc1 = 2.400
+    mass1 = 5000
+    temperature1 = 10
+    shc2 = 1.000
+    mass2 = 4000
+    temperature2 = 40
+    combined_temperature = calculate_combined_temperature(shc1, mass1, temperature1, shc2, mass2, temperature2)
+    assert combined_temperature == 17.5
