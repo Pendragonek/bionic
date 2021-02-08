9fdef03dc1ddfa358223864329c7c9cc7c60f4a1
Pendragonek <mateuszlekner@gmail.com>
2021-02-08 20:40:08 +0100

    Implement calculating temperature delta

--- bionic/heat.py
+++ bionic/heat.py
@@ -2,5 +2,10 @@
 
 
 def calculate_heat_amount(shc: float, mass: float, temperature: float) -> float:
-    """Calculate heat amount based on heat capacity, mass and temperature"""
+    """Calculate heat amount based on SHC, mass and temperature"""
     return shc * mass * temperature
+
+
+def calculate_temperature_delta(shc: float, mass: float, heat_amount: float) -> float:
+    """Calculate temperature delta based on SHC, mass and heat transferred"""
+    return heat_amount / (shc * mass)
--- tests/test_heat.py
+++ tests/test_heat.py
@@ -1,5 +1,5 @@
 """Test heat calculations"""
-from bionic.heat import calculate_heat_amount
+from bionic.heat import calculate_heat_amount, calculate_temperature_delta
 
 
 def test_calculate_heat_amount():
@@ -8,3 +8,11 @@ def test_calculate_heat_amount():
     temperature = 10
     heat_amount = calculate_heat_amount(shc, mass, temperature)
     assert heat_amount == 41790
+
+
+def test_calculate_temperature_delta():
+    shc = 2.400
+    mass = 1000
+    heat_amount = 6000
+    temperature_delta = calculate_temperature_delta(shc, mass, heat_amount)
+    assert temperature_delta == 2.5
